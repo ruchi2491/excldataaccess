336605d6309f06082fe05d878fe6cbb37421b177
ruchi2491 <ruchira.more@atmecs.com>
2017-11-27 16:58:59 +0530

    Update App.java

--- src/main/java/com/exceldataaccess/App.java
+++ src/main/java/com/exceldataaccess/App.java
@@ -14,7 +14,7 @@ import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterTest;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
